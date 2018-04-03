423478bdf7409799245dd0921f21b910d72e59c6
Sharp <jake.sharp@skanska.co.uk>
2018-04-03 11:16:57 +0100

    Project Files
    Adding Base project files to folder

---
+++ Assets/MaterialController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+
+    public GameObject floorObject;
+    public Material[] materials = new Material[5]; //for 5 materials
+
+    public void changeMaterial(int index)
+    {
+        if (index >= materials.Length) return; //Checking the material is in the list
+        floorObject.GetComponent<MeshRenderer>().material = materials[index];
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ Assets/VRTK/Examples/ExampleResources/Scripts/SceneChanger.cs
@@ -0,0 +1,116 @@
+﻿namespace VRTK.Examples.Utilities
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class SceneChanger : MonoBehaviour
+    {
+        private bool canPress;
+        private VRTK_ControllerReference controllerReference;
+
+        private void Awake()
+        {
+            canPress = false;
+            Invoke("ResetPress", 1f);
+            DynamicGI.UpdateEnvironment();
+        }
+        public bool startMenuPressed;
+
+
+        private bool IsForwardPressed()
+        {
+            if (!VRTK_ControllerReference.IsValid(controllerReference))
+            {
+                return false;
+            }
+            if (canPress &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Trigger, SDK_BaseController.ButtonPressTypes.Press, controllerReference) &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Grip, SDK_BaseController.ButtonPressTypes.Press, controllerReference) &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Touchpad, SDK_BaseController.ButtonPressTypes.Press, controllerReference))
+            {
+                return true;
+            }
+            return false;
+        }
+
+       
+
+       
+
+        private bool IsBackPressed()
+        {
+            if (!VRTK_ControllerReference.IsValid(controllerReference))
+            {
+                return false;
+            }
+
+            if (canPress &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Trigger, SDK_BaseController.ButtonPressTypes.Press, controllerReference) &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Grip, SDK_BaseController.ButtonPressTypes.Press, controllerReference) &&
+                VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.ButtonTwo, SDK_BaseController.ButtonPressTypes.Press, controllerReference))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void ResetPress()
+        {
+            canPress = true;
+        }
+
+        
+
+        //private void Update()
+        //{
+        //    GameObject rightHand = VRTK_DeviceFinder.GetControllerRightHand(true);
+        //    controllerReference = VRTK_ControllerReference.GetControllerReference(rightHand);
+
+        //    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //    int nextSceneIndex = currentSceneIndex;
+        //    GameObject[] m1 = GameObject.FindGameObjectsWithTag("TextureMenu");
+        //    if (IsForwardPressed() || Input.GetKeyDown(KeyCode.Space))
+        //    {
+                
+        //        Debug.Log("cheese");
+        //        foreach (GameObject m in m1)
+        //        {
+        //            m.GetComponent<Canvas>().enabled = false;
+
+        //        }
+                
+        //        //else if (IsBackPressed() || Input.GetKeyUp(KeyCode.Backspace))
+        //        //{
+        //        //    //nextSceneIndex--;
+        //        //    //if (nextSceneIndex < 0)
+        //        //    //    nextSceneIndex = SceneManager.sceneCountInBuildSettings - 1;
+        //        //}
+
+        //        //if (nextSceneIndex == currentSceneIndex)
+        //        //{
+        //        //    return;
+        //        //}
+
+        //        //SceneManager.LoadScene(nextSceneIndex);
+        //    }
+        //    if (IsForwardPressed() || Input.GetKeyDown(KeyCode.Backspace))
+        //    {
+
+        //        Debug.Log("toast");
+        //        foreach (GameObject m in m1)
+        //        {
+        //            m.GetComponent<Canvas>().enabled = true;
+
+        //        }
+
+
+        //        //nextSceneIndex++;
+        //        //if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        //        //    nextSceneIndex = 0;
+        //    }
+        //}
+    }
+}
+        
+
+    
